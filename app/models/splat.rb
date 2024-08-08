fdc0937bc704a9e0a98a9c081855a3c4ed78e9af
Jason Zimdars <jz@37signals.com>
2024-08-07 22:32:42 -0500

    Direct flow for creating new splats

--- app/models/splat.rb
+++ app/models/splat.rb
@@ -5,5 +5,5 @@ class Splat < ApplicationRecord
   enum :color, %w[
     #AF2E1B #CC6324 #3B4B59 #BFA07A #ED8008 #ED3F1C #BF1B1B #736B1E #D07B53
     #736356 #AD1D1D #BF7C2A #C09C6F #698F9C #7C956B #5D618F #3B3633 #67695E
-  ].index_by(&:itself), suffix: true, default: :dodgerblue
+  ].index_by(&:itself), suffix: true, default: "#698F9C"
 end
