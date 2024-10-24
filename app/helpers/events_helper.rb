60f8ae7c4c7c94c60c07e846f9c500a0d388bb78
Jose Farias <jose@37signals.com>
2024-10-23 19:22:26 -0600

    Remove `boost_event_tag` helper

--- app/helpers/events_helper.rb
+++
@@ -1,5 +0,0 @@
-module EventsHelper
-  def boost_event_tag(event)
-    tag.span hidden: true, data: { rollup_target: "boostEvent", creator_id: event.creator_id, creator_name: event.creator_name }
-  end
-end
